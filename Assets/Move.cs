80a770f6e106c2ee3afe2d437c6375ae55e7e71f
JacquesLesur <lesurjacques@gmail.com>
2018-06-20 16:48:15 +0200

    New Tils

---
+++ Assets/Move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ 
+
+public class Move : MonoBehaviour {
+    public GameObject[] deplacables;
+   
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+         var cam = Camera.main.transform.position;
+        foreach (var obj in deplacables)
+        {
+            Moving destination = obj.GetComponent<Moving>();
+            Transform transform = obj.transform;
+
+            float distance = obj.transform.position.x-cam.x;
+            if (distance < destination.distanceFromPlayer)
+            {
+                Vector3 objFuturePos = new Vector3(destination.targetX, destination.targetY, destination.targetZ);
+                // fade between current position and the target position using lerp
+                obj.transform.position = Vector3.Lerp(transform.position, objFuturePos, destination.smoothing * Time.deltaTime);
+            }
+        }
+        
+}
+}
+
---
+++ Assets/Moving.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving: MonoBehaviour
+{
+    private bool isMoved = false;
+    public float targetX;
+    public float targetY;
+    public float targetZ;
+    public float smoothing = 1f;
+    public float distanceFromPlayer;
+
+   
+
+
+}
+
